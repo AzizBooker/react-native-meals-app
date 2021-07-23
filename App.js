3c277acd7da0dc871bc172e7b6b097bdde869c87
AzizBooker <bookeraziz@gmail.com>
2021-07-23 09:02:06 -0400

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Color from './utils/Colors'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: Colors.backgroundColor,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ utils/Colors.js
@@ -0,0 +1,8 @@
+
+const Colors={
+    primary:"#416165",
+    background:"#E8EDED",
+    accent:"#6A99A0"
+}
+
+export default Colors;
